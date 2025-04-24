f3a1ab28ab63051f2abb5d8466ba27b8c58b0296
Anna Sidorova <sidnura04@yandex.ru>
2025-04-24 17:30:00 +0300

    try migrate

---
+++ laravel/app/Models/Comment.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Comment extends Model
+{
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
+}
+
